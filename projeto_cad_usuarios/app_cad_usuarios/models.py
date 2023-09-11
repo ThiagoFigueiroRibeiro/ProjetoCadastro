6b0c53cb35ceef90f1ceb7ad40c66329561ed13a
ThiagoFigueiroRibeiro <thiago.figueiro.ribeiro@gmail.com>
2023-09-11 11:41:11 -0300

    Update models.py

--- projeto_cad_usuarios/app_cad_usuarios/models.py
+++ projeto_cad_usuarios/app_cad_usuarios/models.py
@@ -4,3 +4,6 @@ class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
     idade = models.IntegerField()
+    peso = models.IntegerField()
+    altura = models.IntegerField()
+    
