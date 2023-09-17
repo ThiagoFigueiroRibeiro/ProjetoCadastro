43af1c69c787538dcb74b489b3b6a16ae51b9ab0
ThiagoFigueiroRibeiro <thiago.figueiro.ribeiro@gmail.com>
2023-09-17 14:02:29 -0300

    Update urls.py

--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -6,9 +6,15 @@ urlpatterns = [
     # usuarios.com
     path('',views.home,name='home'),
 
-    # usuarios.com/usuarios
+    # usuarios.com/usuarios - Quando um usuário novo é cadastrado, essa página é exibida.
     path('usuarios/',views.usuarios,name='listagem_usuarios'),
 
-   # usuarios.com/exibir
-    path('exibir/',views.exibir,name='exibir_usuarios')
+   # usuarios.com/exibir - Exibe uma lista com todos os usuários cadastrados.
+    path('exibir/',views.exibir,name='exibir_usuarios'),
+
+    # usuarios.com/pesquisa - Página de pesquisa específica de usuário.
+    path('pesquisa/',views.pesquisa,name='pesquisa_usuarios'),
+
+    # usuarios.com/resultado_pesquisa - Exibe uma lista com os dados de um determinado usuário.
+    path('resultado_pesquisa/',views.resultado_pesquisa,name='resultado_pesquisa'),
 ]
